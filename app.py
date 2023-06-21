f340beec59af3b08d9b9a0c4362a291f3e1382b1
ayush-that <ayush1337@hotmail.com>
2023-06-21 14:52:28 +0530

    Stage 1 of dev

---
+++ app.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+import PyPDF2
+from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfile
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width=600, height=300)
+canvas.grid(columnspan=3, rowspan=3)
+
+#logo
+logo = Image.open('logo.png')
+logo = ImageTk.PhotoImage(logo)
+logo_label = tk.Label(image=logo)
+logo_label.image = logo
+logo_label.grid(column=1, row=0)
+
+#instructions
+instructions = tk.Label(root, text="Select a PDF file on your computer to extract all its text", font="Serif")
+instructions.grid(columnspan=3, column=0, row=1)
+
+def open_file():
+    browse_text.set("loading...")
+    file = askopenfile(parent=root, mode='rb', title="Choose a file", filetype=[("Pdf file", "*.pdf")])
+    if file:
+        print("file was successfully loaded")
+
+#browse button
+browse_text = tk.StringVar()
+browse_btn = tk.Button(root, textvariable=browse_text, command=lambda:open_file(), font="Serif", bg="#20bebe", fg="white", height=2, width=15)
+browse_text.set("Browse")
+browse_btn.grid(column=1, row=2)
+
+canvas = tk.Canvas(root, width=600, height=250)
+canvas.grid(columnspan=3)
+
+root.mainloop()
