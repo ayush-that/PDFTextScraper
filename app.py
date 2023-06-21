0bcc0ad1d174b7bc97837c785b82e5f732f60145
ayush-that <ayush1337@hotmail.com>
2023-06-21 17:02:49 +0530

    .

--- app.py
+++ app.py
@@ -21,13 +21,25 @@ instructions.grid(columnspan=3, column=0, row=1)
 
 def open_file():
     browse_text.set("loading...")
-    file = askopenfile(parent=root, mode='rb', title="Choose a file", filetype=[("Pdf file", "*.pdf")])
+    file = askopenfile(parent=root, mode='rb', title="Choose a file", filetypes=[('Pdf file', '*.pdf')])
     if file:
-        print("file was successfully loaded")
+        read_pdf = PyPDF2.PdfFileReader(file)
+        page = read_pdf.getPage(0)
+        page_content = page.extractText()
+
+        #text box
+        text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
+        text_box.insert(1.0, page_content)
+        text_box.tag_configure("center", justify="center")
+        text_box.tag_add("center", 1.0, "end")
+        text_box.grid(column=1, row=3)
+
+        browse_text.set("Browse")
 
 #browse button
 browse_text = tk.StringVar()
-browse_btn = tk.Button(root, textvariable=browse_text, command=lambda:open_file(), font="Serif", bg="#20bebe", fg="white", height=2, width=15)
+browse_btn = tk.Button(root, textvariable=browse_text, command=lambda: open_file(), font="Serif", bg="#20bebe", fg="white", height=2, width=15)
+
 browse_text.set("Browse")
 browse_btn.grid(column=1, row=2)
 
